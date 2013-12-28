14e59d7ca72868c625e59d9139a049c25ebaead9
Abraham Xiao <q289196573@gmail.com>
2013-12-28 15:08:03 +0400

    Let the hunt begin. Why research? Because research shall provide you with happiness. "Do you have a life?" "No."

---
+++ Archive/auto/Project-Note.el
@@ -0,0 +1,15 @@
+(TeX-add-style-hook "Project-Note"
+ (lambda ()
+    (TeX-run-style-hooks
+     "hyperref"
+     "url"
+     "graphicx"
+     "algorithmic"
+     "algorithm"
+     "amssymb"
+     "amsmath"
+     "latex2e"
+     "art10"
+     "article"
+     "")))
+
---
+++ Codes/multi_variate.m
@@ -0,0 +1,5 @@
+%% Description
+% A file that reads daily commute data and tries to fit it (if not
+% for many days...) in the multivariate model.
+
+%% Implementation
