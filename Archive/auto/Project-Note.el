61cffe9ad03d81231b8199530a28d8418640eb4d
Abraham Xiao <q289196573@gmail.com>
2013-12-28 17:06:12 +0400

    TODO lists updated. I can never under estimate this thesis too much. Anyway, in this situation I am a fire fighter.
    For one thing, I hate the "No-Relationship" environment here. Why do not
    allow people to, say have a so-called "love freedom" just like China?

--- Archive/auto/Project-Note.el
+++ Archive/auto/Project-Note.el
@@ -1,8 +1,12 @@
 (TeX-add-style-hook "Project-Note"
  (lambda ()
+    (LaTeX-add-labels
+     "sec:introduction"
+     "sec:multi-variate-model")
     (TeX-run-style-hooks
      "hyperref"
      "url"
+     "listings"
      "graphicx"
      "algorithmic"
      "algorithm"
